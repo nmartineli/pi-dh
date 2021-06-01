2e5d854d92a5b342e541f426f829acb2d2148d28
denisgodoy <denisgodoy1@gmail.com>
2021-06-01 20:22:57 -0300

    desafio corrigo

--- backend/controllers/DocenteCursoController.js
+++ backend/controllers/DocenteCursoController.js
@@ -4,7 +4,6 @@ const DocenteCursoController = {
     index: async (req, res) => {
         const { id } = req.params;
         const results = await DocenteCursoService.getDocenteCurso(id);
-        console.log(results[0].nome);
         return res.send(results);
     }
 }
--- backend/services/DocenteCursoService.js
+++ backend/services/DocenteCursoService.js
@@ -2,9 +2,10 @@ const database = require('../database/models/index');
 
 const DocenteCursoService = {
 	getDocenteCurso: async (id) => {
-		const results = await database.Docente.findAll({
+		const results = await database.Docente.findOne({
 			where: { idDocente : id },
-			include: database.Curso
+			attributes: [ "nome" ],
+			include: [ { model: database.Curso, attributes: ["titulo"] } ]
 		});
 		return results;
 	},
